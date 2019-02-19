59985bb3220caed16dc0546325880dfa75f6af95
MgilyA <zq6295migly@gmail.com>
2019-02-19 20:06:42 +0900

    [update]GA仮実装

--- src/Method/GeneticAlgorithm.py
+++ src/Method/GeneticAlgorithm.py
@@ -21,22 +21,21 @@ class GeneticAlgorithm:
             sample.append(random.sample(target_ids, len(target_ids)))
         return sample
 
-    '''
-    def sort_genome(genomes):
+    def sort_genome(self, genomes):
         sorted_genomes = sorted(genomes, key=lambda genomes: sum(genomes[:]), reverse=True)
         return sorted_genomes
 
-    def achievement(sample):
+    def achievement(self, sample):
         # すべての要素が1であれば1.0を返す
         # 平均
         return sum(sample) / len(sample)
 
-    def get_genome_data(genomes):
+    def get_genome_data(self, genomes):
         sum = 0
         max = 0
         min = 1
         for genome in genomes:
-            ach = achievement(genome)
+            ach = self.achievement(genome)
             sum += ach
             if ach > max:
                 max = ach
@@ -45,12 +44,12 @@ class GeneticAlgorithm:
 
         return max, sum / len(genomes), min
 
-    def select_elite(genomes, rate):
+    def select_elite(self, genomes, rate):
         # ルーレット
         list = []
         G = 0
         for genome in genomes:
-            ach = achievement(genome)
+            ach = self.achievement(genome)
             list.append(ach)
             G += ach
 
@@ -68,7 +67,7 @@ class GeneticAlgorithm:
                 i = 0
         return result
 
-    def fusion(sample1, sample2):
+    def fusion(self, sample1, sample2):
         limit = random.randint(0, len(sample1))
         tmp1 = copy.copy(sample1)
         tmp2 = copy.copy(sample2)
@@ -77,23 +76,29 @@ class GeneticAlgorithm:
             # tmp2[i] = sample1[i]
         return tmp1
 
-    def mutation(genome, probability):
+    def mutation(self, genome, probability):
         for i in range(len(genome)):
             if np.random.choice([True, False], p=[probability, 1 - probability]):
                 genome[i] = 2 + ~genome[i]  # not演算
         return genome
 
-    def generate_next(current_genomes):
+    def generate_next(self, current_genomes):
         next_genomes = []
-        elites = select_elite(current_genomes, 0.3)
+        elites = self.select_elite(current_genomes, 0.3)
         next_genomes.append(elites[0])
         while len(next_genomes) < M:
             if np.random.choice([1, 0], p=[0.5, 0.5]):  # 交配確率
                 sample = random.sample(elites, 2)
-                result1 = fusion(sample[0], sample[1])
-                next_genomes.append(mutation(result1, MUTANT_RATE))
+                result1 = self.fusion(sample[0], sample[1])
+                next_genomes.append(self.mutation(result1, MUTANT_RATE))
         return next_genomes
-        '''
 
     def calc(self, targets: list):
-        self.set_genome(targets)
+        # 遺伝子生成
+        genomes = self.set_genome(targets)
+
+        # T世代ループ
+        for t in range(T):
+            sorted_genomes = self.sort_genome(genomes)
+            genomes = self.generate_next(sorted_genomes)
+            result = self.get_genome_data(genomes)
