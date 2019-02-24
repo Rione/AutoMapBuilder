b470f1d1c8415f8087580273094edb1e4fd0e95c
MgilyA <zq6295migly@gmail.com>
2019-02-24 17:44:49 +0900

    [update]関数分離（GA

--- src/Method/GeneticAlgorithm.py
+++ src/Method/GeneticAlgorithm.py
@@ -281,9 +281,9 @@ class GeneticAlgorithm:
         plt.plot(max_g, label='max')
         plt.plot(ave_g, label='average')
         plt.plot(min_g, label='min')
-        plt.show()
-        # plt.savefig('./image/test' + datetime.now().strftime("%Y%m%d-%H%M%S") + '.png')
-        # plt.cla()
+        plt.savefig('./image/test' + datetime.now().strftime("%Y%m%d-%H%M%S") + '.png')
+        # plt.show()
+        plt.cla()
 
         result = self.sort_genome(genomes)[0][1]
         result.append(result[0])
--- src/ga_main.py
+++ src/ga_main.py
@@ -1,9 +1,6 @@
-import copy
-import os, sys
+import os, sys, copy
 
 sys.path.append(os.getcwd().replace('/src', ''))
-
-import time
 
 from src import MapReader, ScenarioReader, GraphDrawer, Astar
 
@@ -13,20 +10,22 @@ from src.Method import GeneticAlgorithm
 
 MAP_NAME = 'sakae'
 
-if __name__ == '__main__':
-    map = MapReader.MapReader(MAP_NAME)
-    reader = ScenarioReader.ScenarioReader(MAP_NAME)
+map = MapReader.MapReader(MAP_NAME)
+reader = ScenarioReader.ScenarioReader(MAP_NAME)
 
-    scenarios = reader.scenario_reader()
-    world_info = map.build_map()
-    graph_info = map.build_graph()
+world_info = map.build_map()
+graph_info = map.build_graph()
+drawer = GraphDrawer.GraphDrawer(world_info.g_nodes)
 
+location_ids = []
+
+
+def main():
     astar = Astar.Astar(map.world_info.g_nodes)
+    scenarios = reader.scenario_reader()
     greedy = Greedy.Greedy(map.world_info)
-    drawer = GraphDrawer.GraphDrawer(world_info.g_nodes)
 
     # 市民の除くエージェント＆避難所をリストアップ
-    location_ids = []
     for scenario in scenarios:
         # 避難所
         if scenario[0] == 'refuge':
@@ -42,28 +41,24 @@ if __name__ == '__main__':
             location_ids.append(scenario[1])
 
     # result = greedy.calc(location_ids, '')
-    r_list = []
-    for i in range(10):
-        # 遺伝的アルゴリズム
-        genetic = GeneticAlgorithm.GeneticAlgorithm(map.world_info)
-        genetic_route = genetic.calc(location_ids)
+    # 遺伝的アルゴリズム
+    genetic = GeneticAlgorithm.GeneticAlgorithm(map.world_info)
+    genetic_route = genetic.calc(location_ids)
 
-        # 2-opt法
-        # two_opt = TwoOpt.TwoOpt(map.world_info)
-        # two_opt_route = two_opt.calc(genetic_route, '')
+    # 2-opt法
+    # two_opt = TwoOpt.TwoOpt(map.world_info)
+    # two_opt_route = two_opt.calc(genetic_route, '')
 
-        route = astar.interpolation(genetic_route)
-        print(route[0])
-        r_list.append(route[0])
-        e
-        #with open('./t_pmx', mode='a') as f:
-        #    f.writelines(str(route[0]) + '\n')
-'''
+    route = astar.interpolation(genetic_route)
+    print(route[0])
+    print(route[1])
+    return route
 
+
+if __name__ == '__main__':
+    route = main()
     drawer.map_register(graph_info.branch_list)
     drawer.route_register(route[1])
     for id in location_ids:
         drawer.node_register(id)
     drawer.show_plt()
-
-'''
