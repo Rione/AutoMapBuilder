edc092173ec93c21555daec4c1e2935da7594b14
MgilyA <zq6295migly@gmail.com>
2019-02-24 18:21:57 +0900

    [upadte]ga_one_oderデータ取り50回

--- src/sample_exe.py
+++ src/sample_exe.py
@@ -5,4 +5,10 @@ sys.path.append(os.getcwd().replace('/src', ''))
 from src import ga_main, greedy_main, two_opt_main
 
 if __name__ == '__main__':
-    print(greedy_main.main())
+    sample = []
+    for i in range(50):
+        print(i)
+        sample.append(ga_main.main()[0])
+    with open(os.getcwd() + '/result', mode='w') as f:
+        for s in sample:
+            f.write(str(s) + '\n')
