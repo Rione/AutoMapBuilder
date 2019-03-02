ed7b152051c6e01134193f8b037f6facd582518b
MgilyA <zq6295migly@gmail.com>
2019-03-02 09:28:01 +0900

    [update]edge書き出し

--- src/GMLWriter.py
+++ src/GMLWriter.py
@@ -9,12 +9,14 @@ class GMLWrite:
 
     def write(self, building_list: dict, road_list: dict):
         nodes = {}
+        edges = {}
         for building_id in building_list:
             for edge_id in building_list[building_id].edges:
                 f_node = building_list[building_id].edges[edge_id].first
                 nodes.setdefault(f_node.id, f_node)
                 e_node = building_list[building_id].edges[edge_id].end
                 nodes.setdefault(e_node.id, e_node)
+                edges.setdefault(edge_id, building_list[building_id].edges[edge_id])
 
         for road_id in road_list:
             for edge_id in road_list[road_id].edges:
@@ -22,6 +24,7 @@ class GMLWrite:
                 nodes.setdefault(f_node.id, f_node)
                 e_node = road_list[road_id].edges[edge_id].end
                 nodes.setdefault(e_node.id, e_node)
+                edges.setdefault(edge_id, road_list[road_id].edges[edge_id])
 
         doc = xml.dom.minidom.Document()
 
@@ -31,7 +34,6 @@ class GMLWrite:
 
         nodelist = doc.createElement('rcr:nodelist')
         root.appendChild(nodelist)
-
         # node書き出し
         for node_id in nodes:
             Node = doc.createElement('gml:Node gml:id="' + str(node_id) + '"')
@@ -44,5 +46,16 @@ class GMLWrite:
             Point.appendChild(coordinates)
             coordinates.appendChild(
                 doc.createTextNode(str(float(nodes[node_id].x)) + ',' + str(float(nodes[node_id].y))))
+
+        edgelist = doc.createElement('rcr: edgelist')
+        root.appendChild(edgelist)
+        # edge書き出し
+        for edge_id in edges:
+            Edge = doc.createElement('gml:Edge gml:id="' + str(edge_id) + '"')
+            edgelist.appendChild(Edge)
+            directedNode = doc.createElement('orientation="-" xlink:href="#' + str(edges[edge_id].first.id) + '"')
+            Edge.appendChild(directedNode)
+            directedNode = doc.createElement('orientation="+" xlink:href="#' + str(edges[edge_id].end.id) + '"')
+            Edge.appendChild(directedNode)
 
         print(doc.toprettyxml())
